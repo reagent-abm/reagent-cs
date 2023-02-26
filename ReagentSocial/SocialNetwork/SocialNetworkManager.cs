67511670b913f85f21bdcb3631a2b29d915ec263
Robert Greener <me@r0bert.dev>
2023-02-26 20:15:12 +0000

    SocialNetworkManager.cs: Document file
    RC-9 #in-progress #time 10m

--- ReagentSocial/SocialNetwork/SocialNetworkManager.cs
+++ ReagentSocial/SocialNetwork/SocialNetworkManager.cs
@@ -1,6 +1,14 @@
 namespace Reagent.Social.SocialNetwork;
 
+/// <summary>
+/// The <c>SocialNetworkManager</c> is a static class that manages the social network.
+///
+/// The <c>SocialNetwork</c> Should be set to the social network in use.
+/// </summary>
 public static class SocialNetworkManager
 {
+    /// <summary>
+    /// The social network instance.
+    /// </summary>
     public static ISocialNetwork? SocialNetwork { get; set; }
 }
